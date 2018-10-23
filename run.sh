fa73d5c6fc2654bd1c16d83ed9157a10e71caecb
lgeorge <lgeorge@companieshouse.gov.uk>
2018-10-23 13:21:07 +0100

    Set embeddedMongoProxy property to allow MongoDB tests to run.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mvn clean package
+mvn -DembeddedMongoProxyHost=wsproxy.internal.ch clean package
 java -Xmx800m -jar target/formsapiservice-1.0-SNAPSHOT.jar server configuration.yml
